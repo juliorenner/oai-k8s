2c0ac410a4de6fbeba597ebacb23399e4a419a0d
Julio Renner <julio.renner@sap.com>
2020-09-20 16:02:26 -0300

    fix: port name

--- operator/controllers/split_controller.go
+++ operator/controllers/split_controller.go
@@ -537,7 +537,7 @@ func getContainerPorts(split SplitPiece) []v1.ContainerPort {
 	containerPorts := []v1.ContainerPort{}
 	for _, port := range ports {
 		containerPort := v1.ContainerPort{
-			Name:          fmt.Sprintf("port-%d", port),
+			Name:          fmt.Sprintf("port-%d", port.number),
 			HostPort:      port.number,
 			ContainerPort: port.number,
 			Protocol:      port.protocol,
