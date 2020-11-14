a673348fa6461a6a2eaf6cbe7a35a4a47b556a90
Julio Renner <julio.renner@sap.com>
2020-11-14 19:38:05 -0300

    undo

--- tests/main.py
+++ tests/main.py
@@ -174,27 +174,28 @@ def main():
 
     output_start_end_times("start")
 
-    # if args.resources_validation:
-    #     output_start_end_times("resources validation - inicio")
-    #     for i in range(1, 7):
-    #         TestSplits(args.number_of_executions,
-    #                 f"resources-{i}.yaml", args.resources_validation)        
-    #     output_start_end_times("resources validation - fim")
+    if args.resources_validation:
+        output_start_end_times("resources validation - inicio")
+        for i in range(1, 7):
+            TestSplits(args.number_of_executions,
+                    f"resources-{i}.yaml", args.resources_validation)        
+        output_start_end_times("resources validation - fim")
 
-    # for i in range(1, 7):
-    #     TestSplitsPlacer(args.number_of_executions,
-    #                     f"bw-max-delay-min-{i*3}.yaml")
+    for i in range(1, 7):
+        TestSplitsPlacer(args.number_of_executions,
+                        f"bw-max-delay-min-{i*3}.yaml")
+
     for i in range(1, 7):
         TestSplitsPlacer(args.number_of_executions,
                         f"bw-random-link-delay-{i*3}.yaml")
 
-    # for i in range(1, 7):
-    #     TestSplitsPlacer(args.number_of_executions,
-    #                     f"bw-min-link-delay-{i*3}.yaml")
+    for i in range(1, 7):
+        TestSplitsPlacer(args.number_of_executions,
+                        f"bw-min-link-delay-{i*3}.yaml")
 
-    # for i in range(1, 7):
-    #     TestSplits(args.number_of_executions,
-    #             f"scheduler-{i*3}.yaml")
+    for i in range(1, 7):
+        TestSplits(args.number_of_executions,
+                f"scheduler-{i*3}.yaml")
 
     output_start_end_times("end")
 
